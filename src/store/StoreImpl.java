2a019451f11aebc7844ff8f4ea58f3cfdca2b649
Iker Blanco <iker@obsidiansolutions.es>
2023-11-26 22:58:45 +0100

    chore: add missing docstrings

--- src/store/StoreImpl.java
+++ src/store/StoreImpl.java
@@ -11,18 +11,30 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.logging.Logger;
 
+/**
+ * Implementation of StoreInterface providing functionalities for a store.
+ * This includes user authentication, product management, and data persistence using JSON files.
+ */
 public class StoreImpl implements StoreInterface {
     private static final Logger logger = CustomLogger.getLogger(StoreImpl.class);
 
     private static final String USERS_JSON = "users.json";
     private static final String PRODUCTS_JSON = "products.json";
 
+    /**
+     * Constructor for StoreImpl.
+     * Initializes the store by ensuring necessary JSON files exist.
+     */
     public StoreImpl() {
-        // Initialize the store with default files if they don't exist
         initializeFile(USERS_JSON);
         initializeFile(PRODUCTS_JSON);
     }
 
+    /**
+     * Initializes a file if it does not already exist.
+     *
+     * @param filePath the path to the file to be initialized.
+     */
     private void initializeFile(String filePath) {
         try {
             logger.info("Initializing file: " + filePath);
@@ -30,19 +42,23 @@ public class StoreImpl implements StoreInterface {
             Files.write(Paths.get(filePath), "[]".getBytes(), StandardOpenOption.CREATE);
         } catch (IOException e) {
             logger.info("File already exists: " + filePath);
-            // File already exists or another IO error occurred
         }
     }
 
+    /**
+     * Authenticates a user by checking the provided username and password against the stored data.
+     *
+     * @param username the username to authenticate.
+     * @param password the password to authenticate.
+     * @return true if authentication is successful, false otherwise.
+     */
     @Override
     public boolean doUserLogin(String username, String password) {
         logger.info("Logging in user: " + username);
         JSONArray users = JsonUtil.readJsonArrayFromFile(USERS_JSON);
         for (Object o : users) {
             JSONObject user = (JSONObject) o;
-            String storedUsername = (String) user.get("username");
-            String storedPassword = (String) user.get("password");
-            if (storedUsername.equals(username) && storedPassword.equals(password)) {
+            if (username.equals(user.get("username")) && password.equals(user.get("password"))) {
                 logger.info("User logged in: " + username);
                 return true;
             }
@@ -51,6 +67,15 @@ public class StoreImpl implements StoreInterface {
         return false;
     }
 
+    /**
+     * Registers a new user with the provided details.
+     *
+     * @param username the username of the new user.
+     * @param password the password of the new user.
+     * @param email    the email of the new user.
+     * @param phone    the phone number of the new user.
+     * @return true if registration is successful.
+     */
     @Override
     public boolean registerNewUser(String username, String password, String email, String phone) {
         logger.info("Registering new user: " + username);
@@ -67,6 +92,13 @@ public class StoreImpl implements StoreInterface {
         return true;
     }
 
+    /**
+     * Inserts a new product into the shop.
+     *
+     * @param name  the name of the product.
+     * @param price the price of the product.
+     * @return true if the product is successfully inserted.
+     */
     @Override
     public boolean insertProductInShop(String name, double price) {
         logger.info("Inserting product: " + name + " with price: " + price);
@@ -81,21 +113,30 @@ public class StoreImpl implements StoreInterface {
         return true;
     }
 
+    /**
+     * Retrieves and shows all products in the shop.
+     *
+     * @return JSONArray containing all the products.
+     */
     @Override
     public JSONArray showProductsInShop() {
         logger.info("Showing all products");
         return JsonUtil.readJsonArrayFromFile(PRODUCTS_JSON);
     }
 
+    /**
+     * Deletes a product from the shop.
+     *
+     * @param productName the name of the product to delete.
+     * @return true if the product is successfully deleted.
+     */
     @Override
     public boolean deleteProductInShop(String productName) {
         logger.info("Deleting product: " + productName);
         JSONArray products = JsonUtil.readJsonArrayFromFile(PRODUCTS_JSON);
-        products.removeIf(product -> ((JSONObject) product).get("name").equals(productName));
+        products.removeIf(product -> productName.equals(((JSONObject) product).get("name")));
         JsonUtil.writeJsonArrayToFile(products, PRODUCTS_JSON);
         logger.info("Product deleted: " + productName);
         return true;
     }
-
-
 }
